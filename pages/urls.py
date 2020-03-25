96a09542679c8495ce396b5c87da88b5206ee14b
Ophir Taieb <ophir@localhost.localdomain>
2020-03-25 19:16:35 +0200

    Return json response

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_page_view, json_view
 
 urlpatterns = [
-    path('', home_page_view, name='home')
+    path('', home_page_view, name='home'),
+    path('json/', json_view, name='json')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def home_page_view(request):
     return HttpResponse('Hello, World!')
+
+
+def json_view(request):
+    return JsonResponse({'Hello': 'World!'})
