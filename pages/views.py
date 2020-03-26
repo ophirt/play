c9fc1372d6988105923d2e16ee03c1092503fe5b
Ophir Taieb <ophir@localhost.localdomain>
2020-03-26 17:31:44 +0200

    permissions

--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,7 @@ from rest_framework import viewsets
 
 from .serializers import ServerSerializer
 from .models import Server
+from rest_framework import permissions
 
 
 def home_page_view(request):
@@ -17,3 +18,4 @@ def home_page_view(request):
 class ServerViewSet(viewsets.ModelViewSet):
     queryset = Server.objects.all().order_by('name')
     serializer_class = ServerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
